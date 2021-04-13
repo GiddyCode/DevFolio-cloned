d7e188ec1c14e198a1a9997253402b9292e9c250
Ogbonna Gideon <ogbonnagideon1994@gmail.com>
2021-04-13 10:58:19 +0100

    -- changed some constants

---
+++ lib/widget/aboutMeText.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AboutMeText extends StatelessWidget {
+  final TextAlign textAlign;
+  final double fontSize;
+
+  const AboutMeText({Key key, this.textAlign, this.fontSize}) : super(key: key);
+
+  TextStyle _textStyle(double fSize, bool bold) {
+    return GoogleFonts.montserrat(
+      fontSize: fSize ?? 14,
+      fontWeight: !bold ? FontWeight.w100 : FontWeight.w400,
+      letterSpacing: 1.0,
+      height: 2.0,
+      color: Colors.white,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: textAlign ?? TextAlign.left,
+      text: TextSpan(children: [
+        TextSpan(
+          text:
+              "Hi There! I'm Gideon, a Flutter developer, Android developer, and UI/UX Designer based in Lagos, Nigeria.\nI have been developing mobile apps for over 3 years now, I develop apps with appealing UI and robust performance. I have worked in teams for various start-ups (that cuts across Financial sector, Health sector and the Social Media sector) and helped them in launching their products\n\nRight now I'm in currently ",
+          style: MediaQuery.of(context).size.width < 600
+              ? _textStyle(fontSize, false)
+              : _textStyle(fontSize + 2, false),
+        ),
+        TextSpan(
+          text: "developing Bloverse App",
+          style: MediaQuery.of(context).size.width < 600
+              ? _textStyle(fontSize, true)
+              : _textStyle(fontSize + 2, true),
+        ),
+        TextSpan(
+          text: ", active ",
+          style: MediaQuery.of(context).size.width < 600
+              ? _textStyle(fontSize, false)
+              : _textStyle(fontSize + 2, false),
+        ),
+        TextSpan(
+          text: "Co-organising @Google Developer Group Ajah,",
+          style: MediaQuery.of(context).size.width < 600
+              ? _textStyle(fontSize, true)
+              : _textStyle(fontSize + 2, true),
+        ),
+        TextSpan(
+          text: " and",
+          style: MediaQuery.of(context).size.width < 600
+              ? _textStyle(fontSize, false)
+              : _textStyle(fontSize + 2, false),
+        ),
+        TextSpan(
+          text: "solidifying my knowledge in Product Design",
+          style: MediaQuery.of(context).size.width < 600
+              ? _textStyle(fontSize, true)
+              : _textStyle(fontSize + 2, true),
+        )
+      ]),
+    );
+  }
+}
